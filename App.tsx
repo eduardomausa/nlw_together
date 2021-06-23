11afe6afeeb61722633c232fa5d11156325f8ada
Eduardo Mausa <mausaeduardo@gmail.com>
2021-06-23 08:12:00 -0300

    Class two in progress

--- App.tsx
+++ App.tsx
@@ -1,11 +1,11 @@
-import React from 'react';
+import React, { Children } from 'react';
 import { StatusBar } from 'react-native';
 import { Inter_400Regular, Inter_500Medium } from '@expo-google-fonts/inter';
 import { Rajdhani_500Medium, Rajdhani_700Bold } from '@expo-google-fonts/rajdhani';
 import AppLoading from 'expo-app-loading';
 import { useFonts } from 'expo-font';
 
-import { SignIn } from './src/screens/SignIn';
+import { Routes } from './src/routes';
 import { Background } from './src/components/Background';
 
 export default function App() {
@@ -27,7 +27,8 @@ export default function App() {
         backgroundColor="transparent"
         translucent
       />
-      <SignIn />
+
+      <Routes />
     </Background>
   );
 }
