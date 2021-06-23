7207daca675489931d8632b6225d7ed602c76855
Eduardo Mausa <mausaeduardo@gmail.com>
2021-06-22 21:59:34 -0300

    Adding gradient to the SignIn screen

--- App.tsx
+++ App.tsx
@@ -6,6 +6,7 @@ import AppLoading from 'expo-app-loading';
 import { useFonts } from 'expo-font';
 
 import { SignIn } from './src/screens/SignIn';
+import { Background } from './src/components/Background';
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -20,13 +21,13 @@ export default function App() {
   }
 
   return (
-    <>
+    <Background>
       <StatusBar
         barStyle="light-content"
         backgroundColor="transparent"
         translucent
       />
       <SignIn />
-    </>
+    </Background>
   );
 }
